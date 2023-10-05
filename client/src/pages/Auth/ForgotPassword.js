0d5bf40cd85fbc72fd5c9666a13c87f0845ea3dc
GauriNevase <110117024+GauriNevase@users.noreply.github.com>
2023-10-05 21:09:13 +0530

    modified singup login forgotpassword and register

--- client/src/pages/Auth/ForgotPassword.js
+++ client/src/pages/Auth/ForgotPassword.js
@@ -1,92 +1,85 @@
-import React, { useState } from 'react';
-import Layout from '../../components/Layout/Layout';
-import { toast } from 'react-toastify';
-import axios from 'axios';
-import { useNavigate, useLocation } from 'react-router-dom';
+import React, { useState } from "react";
+import Layout from "./../../components/Layout/Layout";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+// import "../../styles/AuthStyles.css";
 
-const ForgotPassword = () => {
-    const [email, setEmail] = useState('');
-    const [newpassword, setNewPassword] = useState('');
-    const [answer, setAnswer] = useState('');
-    const navigate = useNavigate();
-    const location = useLocation();
-   
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+const ForgotPasssword = () => {
+  const [email, setEmail] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [answer, setAnswer] = useState("");
 
-        try {
-            const res = await axios.post(`${process.env.REACT_APP_API}/api/v1/auth/forgot-password`, {
-                email,
-                newpassword,
-                answer,
-            });
+  const navigate = useNavigate();
 
-            const { success, message } = res.data;
+  // form function
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("/api/v1/auth/forgot-password", {
+        email,
+        newPassword,
+        answer,
+      });
+      if (res && res.data.success) {
+        toast.success(res.data && res.data.message);
 
-            if (success) {
-                toast.success(message);
-                
-                localStorage.setItem('auth', JSON.stringify(res.data));
-                navigate('/login'); // Navigate to the login page 
-            } else {
-                toast.error(message);
-            }
-        } catch (error) {
-            console.error(error);
-            toast.error('Something went wrong');
-        }
-    };
+        navigate("/login");
+      } else {
+        toast.error(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong");
+    }
+  };
   return (
-    <Layout title={'forgot-password-Ecommerce'}>
-       <div className="register">
-                <h1>Reset Password</h1>
-                <form onSubmit={handleSubmit}>
-                    <div className="mb-3">
-                        <label htmlFor="email" className="form-label">Email</label>
-                        <input
-                            type="email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            className="form-control"
-                            id="email"
-                            placeholder='Enter your email'
-                            required
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="email" className="form-label">Answer</label>
-                        <input
-                            type="text"
-                            value={answer}
-                            onChange={(e) => setAnswer(e.target.value)}
-                            className="form-control"
-                            id="answer"
-                            placeholder='Enter your secret Answer'
-                            required
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="password" className="form-label">New Password</label>
-                        <input
-                            type="text"
-                            value={newpassword}
-                            onChange={(e) => setNewPassword(e.target.value)}
-                            className="form-control"
-                            id="newpassword"
-                            placeholder='Enter your new password'
-                            required
-                        />
-                    </div>
-                    <div className="mb-3">
-                    {/* <button type="button" className="btn btn-primary" onClick={()=>{navigate('/forgot-password')}}>
-                        Forgot Password
-                    </button> */}
-                    </div>
-                    <button type="submit" className="btn btn-primary">Reset</button>
-                </form>
-            </div>
-    </Layout>
-  )
-}
+    <Layout title={"Forgot Password - Ecommerce APP"}>
+      <div className="form-container ">
+        <form onSubmit={handleSubmit}>
+          <h4 className="title">RESET PASSWORD</h4>
 
-export default ForgotPassword
+          <div className="mb-3">
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="form-control"
+              id="exampleInputEmail1"
+              placeholder="Enter Your Email "
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="text"
+              value={answer}
+              onChange={(e) => setAnswer(e.target.value)}
+              className="form-control"
+              id="exampleInputEmail1"
+              placeholder="Enter Your favorite Sport Name "
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="password"
+              value={newPassword}
+              onChange={(e) => setNewPassword(e.target.value)}
+              className="form-control"
+              id="exampleInputPassword1"
+              placeholder="Enter Your Password"
+              required
+            />
+          </div>
+
+          <button type="submit" className="btn btn-primary">
+            RESET
+          </button>
+        </form>
+      </div>
+    </Layout>
+  );
+};
+
+export default ForgotPasssword;
--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -1,87 +1,91 @@
-import React, { useState } from 'react';
-import Layout from '../../components/Layout/Layout';
-import { toast } from 'react-toastify';
-import axios from 'axios';
-import { useNavigate, useLocation } from 'react-router-dom';
-import { useAuth } from '../../context/auth';
-import Spinner from '../../components/Spinner';
-
+import React, { useState } from "react";
+import Layout from "./../../components/Layout/Layout";
+import axios from "axios";
+import { useNavigate, useLocation } from "react-router-dom";
+import toast from "react-hot-toast";
+import "../../styles/AuthStyles.css";
+import { useAuth } from "../../context/auth";
 const Login = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const navigate = useNavigate();
-    const [auth, setAuth] = useAuth();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [auth, setAuth] = useAuth();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+  const navigate = useNavigate();
+  const location = useLocation();
 
-        try {
-            const response = await axios.post(`${process.env.REACT_APP_API}/api/v1/auth/login`, {
-                email,
-                password,
-            });
+  // form function
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("/api/v1/auth/login", {
+        email,
+        password,
+      });
+      if (res && res.data.success) {
+        toast.success(res.data && res.data.message);
+        setAuth({
+          ...auth,
+          user: res.data.user,
+          token: res.data.token,
+        });
+        localStorage.setItem("auth", JSON.stringify(res.data));
+        navigate(location.state || "/");
+      } else {
+        toast.error(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong");
+    }
+  };
+  return (
+    <Layout title="Register - Ecommer App">
+      <div className="form-container " style={{ minHeight: "90vh" }}>
+        <form onSubmit={handleSubmit}>
+          <h4 className="title">LOGIN FORM</h4>
 
-            const { success, message } = response.data;
+          <div className="mb-3">
+            <input
+              type="email"
+              autoFocus
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="form-control"
+              id="exampleInputEmail1"
+              placeholder="Enter Your Email "
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="form-control"
+              id="exampleInputPassword1"
+              placeholder="Enter Your Password"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <button
+              type="button"
+              className="btn forgot-btn"
+              onClick={() => {
+                navigate("/forgot-password");
+              }}
+            >
+              Forgot Password
+            </button>
+          </div>
 
-            if (success) {
-                toast.success(message);
-                setAuth({
-                    ...auth,
-                    user: response.data.user,
-                    token: response.data.token,
-                });
-                
-                // Redirect to the dashboard based on the user's role
-                navigate(`/dashboard/${response.data.user.role === 1 ? 'admin' : 'user'}`);
-            } else {
-                toast.error(message);
-            }
-        } catch (error) {
-            console.error(error);
-            toast.error('Something went wrong');
-        }
-    };
-    //backend ends
-
-    return (
-        <Layout title={"Login-Ecommerce"}>
-            <div className="register">
-                <h1>Login</h1>
-                <form onSubmit={handleSubmit}>
-                    <div className="mb-3">
-                        <label htmlFor="email" className="form-label">Email</label>
-                        <input
-                            type="email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            className="form-control"
-                            id="email"
-                            placeholder='Enter your email'
-                            required
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="password" className="form-label">Password</label>
-                        <input
-                            type="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            className="form-control"
-                            id="password"
-                            placeholder='Enter your password'
-                            required
-                        />
-                    </div>
-                    <div className="mb-3">
-                    <button type="button" className="btn btn-primary" onClick={()=>{navigate('/forgot-password')}}>
-                        Forgot Password
-                    </button>
-                    </div>
-                    <button type="submit" className="btn btn-primary">Login</button>
-                </form>
-            </div>
-        </Layout>
-    );
+          <button type="submit" className="btn btn-primary">
+            LOGIN
+          </button>
+        </form>
+      </div>
+    </Layout>
+  );
 };
 
 export default Login;
