be6b68591360ed7baf81e886c21ad5c5b0ecb9c7
GauriNevase <110117024+GauriNevase@users.noreply.github.com>
2023-10-06 00:52:49 +0530

    create category

--- client/src/pages/Admin/CreateCategory.js
+++ client/src/pages/Admin/CreateCategory.js
@@ -5,17 +5,19 @@ import toast from "react-hot-toast";
 import axios from "axios";
 import CategoryForm from "../../components/Form/CategoryForm";
 import { Modal } from "antd";
+
 const CreateCategory = () => {
   const [categories, setCategories] = useState([]);
   const [name, setName] = useState("");
-  const [visible, setVisible] = useState(false);
+  const [open, setOpen] = useState(false);
   const [selected, setSelected] = useState(null);
   const [updatedName, setUpdatedName] = useState("");
-  //handle Form
+
+  // Handle Form Submission
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const { data } = await axios.post("/api/v1/category/create-category", {
+      const { data } = await axios.post(`${process.env.REACT_APP_API}/api/v1/category/create-category`, {
         name,
       });
       if (data?.success) {
@@ -25,21 +27,21 @@ const CreateCategory = () => {
         toast.error(data.message);
       }
     } catch (error) {
-      console.log(error);
-      toast.error("somthing went wrong in input form");
+      console.error(error);
+      toast.error("Something went wrong in the input form");
     }
   };
 
-  //get all cat
+  // Get all categories
   const getAllCategory = async () => {
     try {
-      const { data } = await axios.get("/api/v1/category/get-category");
+      const { data } = await axios.get(`${process.env.REACT_APP_API}/api/v1/category/get-category`);
       if (data.success) {
         setCategories(data.category);
       }
     } catch (error) {
-      console.log(error);
-      toast.error("Something wwent wrong in getting catgeory");
+      console.error(error);
+      toast.error("Something went wrong in getting categories");
     }
   };
 
@@ -47,44 +49,47 @@ const CreateCategory = () => {
     getAllCategory();
   }, []);
 
-  //update category
+  // Update category
   const handleUpdate = async (e) => {
     e.preventDefault();
     try {
       const { data } = await axios.put(
-        `/api/v1/category/update-category/${selected._id}`,
+        `${process.env.REACT_APP_API}/api/v1/category/update-category/${selected._id}`,
         { name: updatedName }
       );
       if (data.success) {
         toast.success(`${updatedName} is updated`);
         setSelected(null);
         setUpdatedName("");
-        setVisible(false);
+        setOpen(false);
         getAllCategory();
       } else {
         toast.error(data.message);
       }
     } catch (error) {
-      toast.error("Somtihing went wrong");
+      console.error(error);
+      toast.error("Something went wrong during the update");
     }
   };
-  //delete category
-  const handleDelete = async (pId) => {
+
+  // Delete category
+  const handleDelete = async (categoryId) => {
     try {
       const { data } = await axios.delete(
-        `/api/v1/category/delete-category/${pId}`
+        `${process.env.REACT_APP_API}/api/v1/category/delete-category/${categoryId}`
       );
       if (data.success) {
-        toast.success(`category is deleted`);
-
+        toast.success(`Category is deleted`);
         getAllCategory();
       } else {
         toast.error(data.message);
       }
     } catch (error) {
-      toast.error("Somtihing went wrong");
+      console.error(error);
+      toast.error("Something went wrong while deleting the category");
     }
   };
+
   return (
     <Layout title={"Dashboard - Create Category"}>
       <div className="container-fluid m-3 p-3">
@@ -111,39 +116,37 @@ const CreateCategory = () => {
                 </thead>
                 <tbody>
                   {categories?.map((c) => (
-                    <>
-                      <tr>
-                        <td key={c._id}>{c.name}</td>
-                        <td>
-                          <button
-                            className="btn btn-primary ms-2"
-                            onClick={() => {
-                              setVisible(true);
-                              setUpdatedName(c.name);
-                              setSelected(c);
-                            }}
-                          >
-                            Edit
-                          </button>
-                          <button
-                            className="btn btn-danger ms-2"
-                            onClick={() => {
-                              handleDelete(c._id);
-                            }}
-                          >
-                            Delete
-                          </button>
-                        </td>
-                      </tr>
-                    </>
+                    <tr key={c._id}>
+                      <td>{c.name}</td>
+                      <td>
+                        <button
+                          className="btn btn-primary ms-2"
+                          onClick={() => {
+                            setOpen(true);
+                            setUpdatedName(c.name);
+                            setSelected(c);
+                          }}
+                        >
+                          Edit
+                        </button>
+                        <button
+                          className="btn btn-danger ms-2"
+                          onClick={() => {
+                            handleDelete(c._id);
+                          }}
+                        >
+                          Delete
+                        </button>
+                      </td>
+                    </tr>
                   ))}
                 </tbody>
               </table>
             </div>
             <Modal
-              onCancel={() => setVisible(false)}
+              onCancel={() => setOpen(false)}
               footer={null}
-              visible={visible}
+              visible={open}
             >
               <CategoryForm
                 value={updatedName}
--- client/src/pages/Auth/Register.js
+++ client/src/pages/Auth/Register.js
@@ -1,121 +1,122 @@
-import React, { useState } from "react";
-import Layout from "./../../components/Layout/Layout";
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
-import toast from "react-hot-toast";
-import "../../styles/AuthStyles.css";
+import React, { useState } from 'react'
+import Layout from '../../components/Layout/Layout'
+import {toast} from 'react-toastify'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+
 const Register = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [phone, setPhone] = useState("");
-  const [address, setAddress] = useState("");
-  const [answer, setAnswer] = useState("");
-  const navigate = useNavigate();
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [phone, setPhone] = useState("")
+    const [address, setAddress] = useState("")
+    const [answer, setAnswer] = useState("")
+    const navigate = useNavigate()
 
-  // form function
-  const handleSubmit = async (e) => {
+    //form function
+const handleSubmit=async(e)=>{
     e.preventDefault();
+    // console.log(name,email,password,address,phone);
+    // toast.success("Register Successfully");
     try {
-      const res = await axios.post("/api/v1/auth/register", {
-        name,
-        email,
-        password,
-        phone,
-        address,
-        answer,
-      });
-      if (res && res.data.success) {
-        toast.success(res.data && res.data.message);
-        navigate("/login");
-      } else {
-        toast.error(res.data.message);
-      }
+        const res=await axios.post(`${process.env.REACT_APP_API}/api/v1/auth/register`,{name,email,password,phone,address,answer});  //backend connection
+        if(res.data.success){
+            toast.success(res.data.message)
+            navigate('/login')
+        }else{
+            toast.error(res.data.message)
+        }
     } catch (error) {
-      console.log(error);
-      toast.error("Something went wrong");
+       console.log(error)
+       toast.error('something went wrong') 
     }
-  };
-
-  return (
-    <Layout title="Register - Ecommer App">
-      <div className="form-container" style={{ minHeight: "90vh" }}>
-        <form onSubmit={handleSubmit}>
-          <h4 className="title">REGISTER FORM</h4>
-          <div className="mb-3">
-            <input
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              className="form-control"
-              id="exampleInputEmail1"
-              placeholder="Enter Your Name"
-              required
-              autoFocus
-            />
-          </div>
-          <div className="mb-3">
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="form-control"
-              id="exampleInputEmail1"
-              placeholder="Enter Your Email "
-              required
-            />
-          </div>
-          <div className="mb-3">
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="form-control"
-              id="exampleInputPassword1"
-              placeholder="Enter Your Password"
-              required
-            />
-          </div>
-          <div className="mb-3">
-            <input
-              type="text"
-              value={phone}
-              onChange={(e) => setPhone(e.target.value)}
-              className="form-control"
-              id="exampleInputEmail1"
-              placeholder="Enter Your Phone"
-              required
-            />
-          </div>
-          <div className="mb-3">
-            <input
-              type="text"
-              value={address}
-              onChange={(e) => setAddress(e.target.value)}
-              className="form-control"
-              id="exampleInputEmail1"
-              placeholder="Enter Your Address"
-              required
-            />
-          </div>
-          <div className="mb-3">
-            <input
-              type="text"
-              value={answer}
-              onChange={(e) => setAnswer(e.target.value)}
-              className="form-control"
-              id="exampleInputEmail1"
-              placeholder="What is Your Favorite sports"
-              required
-            />
-          </div>
-          <button type="submit" className="btn btn-primary">
-            REGISTER
-          </button>
-        </form>
-      </div>
-    </Layout>
-  );
 };
+console.log(process.env.REACT_APP_API);
 
-export default Register;
+    return (
+        <Layout title={"Register-Ecommerce"}>
+            <div className="register">
+                <h1>Register</h1>
+                <form onSubmit={handleSubmit}>
+                    <div className="mb-3">
+                        <label htmlFor="exampleInputName" className="form-label">Name</label>
+                        <input
+                            type="text"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            className="form-control"
+                            id="name"
+                            placeholder='Enter your name'
+                            required
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="exampleInputName" className="form-label">Email</label>
+                        <input
+                            type="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            className="form-control"
+                            id="email"
+                            placeholder='Enter your email'
+                            required
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                        <input
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            className="form-control"
+                            id="password"
+                            placeholder='Enter your password'
+                            required
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="exampleInputName" className="form-label">Phone No</label>
+                        <input
+                            type="text"
+                            value={phone}
+                            onChange={(e) => setPhone(e.target.value)}
+                            className="form-control"
+                            id="phone"
+                            placeholder='Enter your phone no'
+                            required
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="exampleInputName" className="form-label">Address</label>
+                        <input
+                            type="text"
+                            value={address}
+                            onChange={(e) => setAddress(e.target.value)}
+                            className="form-control"
+                            id="address"
+                            placeholder='Enter your address'
+                            required
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="exampleInputName" className="form-label">Answer</label>
+                        <input
+                            type="text"
+                            value={answer}
+                            onChange={(e) => setAnswer(e.target.value)}
+                            className="form-control"
+                            id="answer"
+                            placeholder='What is your favourite colour'
+                            required
+                        />
+                    </div>
+
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
+
+            </div>
+        </Layout>
+    )
+}
+
+export default Register
